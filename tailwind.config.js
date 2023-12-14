11907a62314bb3e4365dd21a9b0ec1c1dfe1f259
Tyler Paige <tylerclarkpaige@gmail.com>
2023-12-14 12:03:10 -0500

    Use Roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.js"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"]
+    }
   },
   plugins: [],
 }
